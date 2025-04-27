552d31d0821aa812234422cb85878596d6cb669b
Ajay Rudrawal <rudrawal@avconnect.ai>
2025-04-26 18:05:00 -0700

    Login required decorator

--- sd_main/views.py
+++ sd_main/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from .forms import UploadPolicyForm
 from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'sd_main/dash/notifications.html')
 
+@login_required
 def vehicles(request):
     return render(request, 'sd_main/dash/vehicles.html')
 
+@login_required
 def drivers(request):
     return render(request, 'sd_main/dash/drivers.html')
 
+@login_required
 def upload_policy(request):
     # if this is a POST request we need to process the form data
     if request.method == "POST":
-        # if the post request has a file under the input name 'document', then save the file.
+        # if the post request has a file under the input name 'policy_file', then save the file.
         request_file = request.FILES['policy_file'] if 'policy_file' in request.FILES else None
         if request_file: # save attached file
             fs = FileSystemStorage()
