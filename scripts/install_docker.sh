df7e30af88bea3cc1d880a7390ce234568e44996
Ajay Rudrawal <rudrawal@avconnect.ai>
2025-05-23 13:00:37 -0700

    Docker name

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+# sudo yum install git
 sudo yum update -y
 # Amazon Linux 2
 # sudo amazon-linux-extras install docker
 # Amazon Linux 2023
 sudo yum install -y docker
 sudo service docker start
+# this will be effective on new login
 sudo usermod -a -G docker ec2-user
