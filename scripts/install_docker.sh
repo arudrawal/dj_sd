af7d259638196bef3e19e3495395a3b54533cc82
Ajay Rudrawal <rudrawal@avconnect.ai>
2025-05-23 13:19:21 -0700

    docker compose

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 # sudo yum install git
 sudo yum update -y
+sudo yum install curl
 # Amazon Linux 2
 # sudo amazon-linux-extras install docker
 # Amazon Linux 2023
 sudo yum install -y docker
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 sudo service docker start
 # this will be effective on new login
 sudo usermod -a -G docker ec2-user
