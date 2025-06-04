6f9cdfb32478aa35db8682279bf0e14b0be89302
Ajay Rudrawal <rudrawal@avconnect.ai>
2025-06-04 10:08:33 -0700

    App runner with python 3

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PYTHONPATH=$PYTHONPATH
-python3 manage.py migrate
-python3 data/seed_database.py
-python3 manage.py collectstatic 
+python manage.py migrate
+python data/seed_database.py
+python manage.py collectstatic 
 gunicorn --bind 0.0.0.0:8000 --workers 2 sd_proj.wsgi
